c28c139533992205cd8734c1754ec4c1117f8aca
Lance Teng <adrianlanceteng@gmail.com>
2022-03-28 02:34:53 +0800

    Added Follow platforms and follow stores pages!
    (Or at least their skeletons and models!) They work essentially the same as followGames, and later on we can spend a little time just streamlining them down into one or two files if we'd like! (instead of doing one for each, it'd probably be more efficient process-wise too!)

---
+++ lib/models/storeModel.dart
@@ -0,0 +1,106 @@
+// To parse this JSON data, do
+//
+//     final storeModel = storeModelFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+StoreModel storeModelFromJson(String str) => StoreModel.fromJson(json.decode(str));
+
+String storeModelToJson(StoreModel data) => json.encode(data.toJson());
+
+class StoreModel {
+  StoreModel({
+    required this.count,
+    required this.next,
+    required this.previous,
+    required this.results,
+  });
+
+  int count;
+  dynamic next;
+  dynamic previous;
+  List<Result> results;
+
+  factory StoreModel.fromJson(Map<String, dynamic> json) => StoreModel(
+    count: json["count"],
+    next: json["next"],
+    previous: json["previous"],
+    results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "count": count,
+    "next": next,
+    "previous": previous,
+    "results": List<dynamic>.from(results.map((x) => x.toJson())),
+  };
+}
+
+class Result {
+  Result({
+    required this.id,
+    required this.name,
+    required this.domain,
+    required this.slug,
+    required this.gamesCount,
+    required this.imageBackground,
+    required this.games,
+  });
+
+  int id;
+  String name;
+  String domain;
+  String slug;
+  int gamesCount;
+  String imageBackground;
+  List<Game> games;
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+    id: json["id"],
+    name: json["name"],
+    domain: json["domain"],
+    slug: json["slug"],
+    gamesCount: json["games_count"],
+    imageBackground: json["image_background"],
+    games: List<Game>.from(json["games"].map((x) => Game.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "domain": domain,
+    "slug": slug,
+    "games_count": gamesCount,
+    "image_background": imageBackground,
+    "games": List<dynamic>.from(games.map((x) => x.toJson())),
+  };
+}
+
+class Game {
+  Game({
+    required this.id,
+    required this.slug,
+    required this.name,
+    required this.added,
+  });
+
+  int id;
+  String slug;
+  String name;
+  int added;
+
+  factory Game.fromJson(Map<String, dynamic> json) => Game(
+    id: json["id"],
+    slug: json["slug"],
+    name: json["name"],
+    added: json["added"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "slug": slug,
+    "name": name,
+    "added": added,
+  };
+}
---
+++ lib/pages/followStores.dart
@@ -0,0 +1,221 @@
+import 'dart:convert';
+import 'dart:core';
+import 'package:flutter/cupertino.dart';  // Might not be necessary to import
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:hud/models/storeModel.dart';
+
+Color? bgColor = Colors.grey[900];
+Color? primaryColor = Colors.purple[900];
+Color? accentColor = Colors.purple[700];
+
+
+class FollowStoreList extends StatefulWidget {
+  const FollowStoreList({Key? key}) : super(key: key);
+
+  @override
+  State<FollowStoreList> createState() => _FollowStoreListState();
+}
+
+class _FollowStoreListState extends State<FollowStoreList> {
+  Future<StoreModel>? _storeModel;
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = Text('Stores');
+  TextEditingController searchInputController = TextEditingController();
+  String searchInputString = '';
+  bool isLoading = false;
+  var currentFocus;
+
+  @override
+  void initState() {
+    _storeModel = API_Manager().getStores('PC');
+    super.initState();
+  }
+
+  void callStores(String input){
+    _storeModel = API_Manager().getStores(input);
+    setState(() {
+
+    });
+    // isLoading = true;
+    //Refresh or setstate() here to refresh the games list.
+  }
+
+  unfocus() {
+    currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+
+      appBar: AppBar(
+          title: customSearchBar,
+          backgroundColor: primaryColor,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: (){
+                  setState(() {
+                    if (customIcon.icon == Icons.search) {
+                      customIcon = const Icon(Icons.cancel);
+                      customSearchBar = ListTile(
+                        leading: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        title: TextField(
+                          controller: searchInputController,
+                          onSubmitted: (String value) { callStores(value);
+                          unfocus();
+                          },
+                          //then call setstate to refresh the games list!
+                          decoration: InputDecoration(
+                            hintText: 'ex. Steam',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    } else if (customIcon.icon == Icons.cancel){
+                      searchInputController.clear();
+                      unfocus();
+                      //return title to games, swap icon back to search
+                      customSearchBar = Text('Games!');
+                      customIcon = const Icon(Icons.search);
+                    }
+                  }
+                  );},
+                icon: customIcon)
+          ]
+      ),
+
+      body: Container(
+        child: FutureBuilder<StoreModel>(
+          future: _storeModel,
+
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              isLoading = false;
+              return ListView.builder(
+                  itemCount: snapshot.data.results.length,
+
+                  itemBuilder: (context, index) {
+                    var platform = snapshot.data.results[index];  // This is responsible for going through the querried items from the API
+
+                    return Container(
+                      height: 80,
+                      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),  // Space between each list item
+
+                      child: Row(
+                        children: <Widget>[
+                          Card(
+                            clipBehavior: Clip.antiAlias,
+
+                            // This changes the shape that the image is in
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+
+                            // This displays the image
+                            child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Image.network(
+                                  platform.backgroundImage,
+                                  fit: BoxFit.cover,
+                                )),
+                          ),
+
+                          SizedBox(width: 8),  // Spacer between image and game title
+
+                          Flexible(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              // Game Title
+                              children: <Widget>[
+                                Flexible(
+                                  child: Text(
+                                    platform.name,
+                                    // overflow: TextOverflow.ellipsis,  // This is to make the 2nd line of the name turned into ... instead of showing everything. Commented it out for now since it looks ugly.
+
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+
+                                  ),
+
+                                ),
+
+                              ],
+
+                            ),
+
+                          ),
+
+                        ],
+
+                      ),
+
+                    );
+
+                  });
+
+            } else
+              return Center(child: CircularProgressIndicator());
+          },
+
+        ),
+
+      ),
+
+    );
+
+  }
+}
+
+class API_Manager {
+  Future<StoreModel> getStores(String storeName) async {
+    var client = http.Client();
+    var storeModel;
+    String baseURL = 'https://api.rawg.io/api/platforms?';
+    String searchParam = 'search=';
+    String urlSearchTerms = storeName.trim().toLowerCase().replaceAll(' ','-');
+    String pageSize = '&page_size=10';
+    String apiKey = '&key=88457281eae8421b8395d12d3df566ad';
+    String finalURL = '';
+
+    if (storeName == ''){ //if theres no platform searched, just return a list of popular platforms, DOES NOT CURRENTLY WORK
+      finalURL = baseURL + apiKey + pageSize;
+    } else { //otherwise attempt the search
+      finalURL = baseURL + searchParam + urlSearchTerms + apiKey + pageSize;
+    }
+
+    var uri = Uri.parse(finalURL);
+
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      var jsonMap = jsonDecode(json);
+
+      storeModel = StoreModel.fromJson(jsonMap);
+    }
+    return storeModel;
+  }
+}
