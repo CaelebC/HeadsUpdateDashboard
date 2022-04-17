c2d4f1aeedae81fc753da4ed9d701723f3feff52
CaelebC <caelebcamaro1@gmail.com>
2022-04-18 01:31:07 +0800

    Displayed SteamDB webpage on Sale page

--- lib/pages/sale.dart
+++ lib/pages/sale.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hud/config/style.dart';
+import 'package:hud/components/steamSaleCountdown.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'dart:convert';
+import 'dart:core';
+import 'package:http/http.dart' as http;
 
 
 class Sale extends StatefulWidget {
@@ -13,11 +19,16 @@ class _SaleState extends State<Sale> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      body: Center(
-        child: Text('SALE PAGE'),
+      appBar: AppBar(
+        title: Text('Steam Sale Countdown'),
+        backgroundColor: primaryColor,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
       ),
-
+      body: WebView(
+        initialUrl: 'https://steamdb.info/sales/history/',
+        javascriptMode: JavascriptMode.unrestricted,
+      )
     );
   }
 }
